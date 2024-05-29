9b6d9ba0892f7bc6c693eb9d6cfc568fdcd53597
Yuriy <vnevremen420@gmail.com>
2024-05-29 21:45:46 +0500

    map

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -93,3 +93,169 @@ import {CountUp} from './countUp.min.js';
 		});
 	});
 })();
+
+// Карта (Google Maps)
+(() => {
+	let map, mapElement = document.getElementById("map");
+
+	function initMap() {
+		const position = { lat: 51.089315839958346, lng: 71.41601615952058 };
+
+		const map = new google.maps.Map(mapElement, {
+			zoom: 15,
+			center: position,
+			disableDefaultUI: true,
+			styles: [
+				{
+					"featureType": "all",
+					"elementType": "geometry",
+					"stylers": [
+						{
+							"color": "#202c3e"
+						}
+					]
+				},
+				{
+					"featureType": "all",
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{
+							"gamma": 0.01
+						},
+						{
+							"lightness": 20
+						},
+						{
+							"weight": "1.39"
+						},
+						{
+							"color": "#ffffff"
+						}
+					]
+				},
+				{
+					"featureType": "all",
+					"elementType": "labels.text.stroke",
+					"stylers": [
+						{
+							"weight": "0.96"
+						},
+						{
+							"saturation": "9"
+						},
+						{
+							"visibility": "on"
+						},
+						{
+							"color": "#000000"
+						}
+					]
+				},
+				{
+					"featureType": "all",
+					"elementType": "labels.icon",
+					"stylers": [
+						{
+							"visibility": "off"
+						}
+					]
+				},
+				{
+					"featureType": "landscape",
+					"elementType": "geometry",
+					"stylers": [
+						{
+							"lightness": 30
+						},
+						{
+							"saturation": "9"
+						},
+						{
+							"color": "#29446b"
+						}
+					]
+				},
+				{
+					"featureType": "poi",
+					"elementType": "geometry",
+					"stylers": [
+						{
+							"saturation": 20
+						}
+					]
+				},
+				{
+					"featureType": "poi.park",
+					"elementType": "geometry",
+					"stylers": [
+						{
+							"lightness": 20
+						},
+						{
+							"saturation": -20
+						}
+					]
+				},
+				{
+					"featureType": "road",
+					"elementType": "geometry",
+					"stylers": [
+						{
+							"lightness": 10
+						},
+						{
+							"saturation": -30
+						}
+					]
+				},
+				{
+					"featureType": "road",
+					"elementType": "geometry.fill",
+					"stylers": [
+						{
+							"color": "#193a55"
+						}
+					]
+				},
+				{
+					"featureType": "road",
+					"elementType": "geometry.stroke",
+					"stylers": [
+						{
+							"saturation": 25
+						},
+						{
+							"lightness": 25
+						},
+						{
+							"weight": "0.01"
+						}
+					]
+				},
+				{
+					"featureType": "water",
+					"elementType": "all",
+					"stylers": [
+						{
+							"lightness": -20
+						}
+					]
+				}
+			]
+		});
+
+		var marker1 = new google.maps.Marker({
+			position: new google.maps.LatLng(51.09237258687083, 71.41140018432453),
+			map: map,
+			title: 'Congress Center'
+		});
+
+		var marker2 = new google.maps.Marker({
+			position: new google.maps.LatLng(51.09290359119403, 71.42210347720625),
+			map: map,
+			title: 'Expo International Exhibition Center'
+		});
+	}
+
+	if (mapElement) initMap();
+})();
